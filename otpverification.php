c64eff1845928927a3ae1478d23faf59cd8ecf0d
nishchayagupta <nishchayagupta@gmail.com>
2018-10-04 16:48:26 -0400

    otp verification added

---
+++ otpverification.php
@@ -0,0 +1,39 @@
+<?php
+
+session_start();
+$otp = $_POST['otpverificationOTP'];
+$username = $_SESSION['username'];
+//setup the request, you can also use CURLOPT_URL
+$ch = curl_init('https://lunar-living.herokuapp.com/otpVerification');
+
+// Returns the data/output as a string instead of raw data
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+// Good practice to let people know who's accessing their servers. See https://en.wikipedia.org/wiki/User_agent
+curl_setopt($ch, CURLOPT_USERAGENT, 'YourScript/0.1 (contact@email)');
+
+//Set your auth headers
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json',
+    "username: $username",
+    "otp: $otp"
+    ));
+
+// get stringified data/output. See CURLOPT_RETURNTRANSFER
+$data = curl_exec($ch);
+
+// get info about the request
+$info = curl_getinfo($ch);
+
+if($data == 'false'){
+  echo "User Not Found";
+}else {
+  echo "Welcome Page";
+  session_destroy();
+}
+
+// close curl resource to free up system resources
+curl_close($ch);
+
+
+ ?>
