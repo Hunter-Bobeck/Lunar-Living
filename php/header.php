70d450a7f703fe2488742ec3340022219d9b28d2
nishchayagupta <nishchayagupta@gmail.com>
2018-11-07 16:56:20 -0500

    created header file and added to all php files

---
+++ php/header.php
@@ -0,0 +1,26 @@
+<head>
+    <!-- metadata -->
+    <meta charset='utf-8'>
+    <!-- Unicode characterset -->
+    <meta http-equiv='X-UA-Compatible' content='IE=epdge'>
+    <!-- Internet Explorer compatibility -->
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <!-- renders the page according to the device width -->
+    <meta name='description' content='Rent a living space, in outer space.'>
+    <!-- description [shown on searchengine result] -->
+    <meta name='keywords' content='Kidd, Living, space, rental, service, management, system, SpaceX, outer, Elon, Musk'>
+    <!-- keywords for searchengine optimization -->
+    <meta name='author' content='IU SE G2'>
+    <!-- author name -->
+    <link rel='icon' href='../images\Icon.ico'>
+    <!-- favicon -->
+    <title>Lunar Living | New Lease</title>
+    <!-- title -->
+
+    <!-- CSS -->
+    <link rel='stylesheet' href='../css/reset.css'>		<!-- Reset CSS -->
+    <link rel='stylesheet' href='../css/bootstrap.min.css'>		<!-- Bootstrap CSS -->
+    <link rel='stylesheet' href='../css/fontawesome.min.css'>		<!-- Font Awesome CSS -->
+    <link rel='stylesheet' href='../css/daterangepicker.css'>		<!-- Date Range Picker CSS -->
+    <link rel='stylesheet' href='../css/style.css'>		<!-- custom CSS -->
+</head>
--- php/maintenance.php
+++ php/maintenance.php
@@ -1,25 +1,6 @@
 <!doctype html>
 <html lang='en'>
-<head>
-	<!-- metadata -->
-    <meta charset='utf-8'>	<!-- Unicode characterset -->
-    <meta http-equiv='X-UA-Compatible' content='IE=epdge'>	<!-- Internet Explorer compatibility -->
-    <meta name='viewport' content='width=device-width, initial-scale=1'>	<!-- renders the page according to the device width -->
-    <meta name='description' content='Rent a living space, in outer space.'>		<!-- description [shown on searchengine result] -->
-    <meta name='keywords' content='Kidd, Living, space, rental, service, management, system, SpaceX, outer, Elon, Musk'>		<!-- keywords for searchengine optimization -->
-    <meta name='author' content='IU SE G2'>		<!-- author name -->
-    <link rel='icon' href='../images\Icon.ico'>		<!-- favicon -->
-
-    <title>Lunar Living | Maintenance</title>		<!-- title -->
-
-	<!-- CSS -->
-	<link rel='stylesheet' href='../css/reset.css'>		<!-- Reset CSS -->
-    <link rel='stylesheet' href='../css/bootstrap.min.css'>		<!-- Bootstrap CSS -->
-    <link rel='stylesheet' href='../css/fontawesome.min.css'>		<!-- Font Awesome CSS -->
-	<link rel='stylesheet' href='../css/style.css'>		<!-- custom CSS -->
-	<link rel='stylesheet' href='../css/style-muuri.css'>		<!-- custom CSS for Muuri -->
-
-</head>
+<?php include 'header.php'; ?>
 <body class='background background-lunar-landed'>
 	<main>
 		<nav class='navbar navbar-expand-lg navbar-light bg-light'>
