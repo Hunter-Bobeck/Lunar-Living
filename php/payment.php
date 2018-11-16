30395ce3e27b3724eb7ae9f09c53754eca34f929
szpatel <szpatel@iu.edu>
2018-11-15 22:33:50 -0500

    Payment API

--- php/payment.php
+++ php/payment.php
@@ -9,8 +9,38 @@ session_start();
 <body class="background background-dark">
     <?php
 
-		$username = $_SESSION['username'];
-		$first_name = $_SESSION['firstName'];
+        $username = $_SESSION['username'];
+        $first_name = $_SESSION['firstName'];
+        //setup the request, you can also use CURLOPT_URL
+        $ch = curl_init('https://lunar-living.herokuapp.com/getUserLease');
+
+        // Returns the data/output as a string instead of raw data
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        // Good practice to let people know who's accessing their servers. See https://en.wikipedia.org/wiki/User_agent
+        curl_setopt($ch, CURLOPT_USERAGENT, 'YourScript/0.1 (contact@email)');
+
+        //Set your auth headers
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json',
+            "username: $username"
+            ));
+
+        // get stringified data/output. See CURLOPT_RETURNTRANSFER
+        $data = curl_exec($ch);
+
+        // get info about the request
+        $info = curl_getinfo($ch);
+
+        if($data == 'false'){
+        echo "User Not Found";
+        }
+        else {
+            $apiData = json_decode($data);
+        }
+
+        // close curl resource to free up system resources
+        curl_close($ch);
 	?>
 	<main class = "content_body">
         <div class='container-fluid padding-zero'>
