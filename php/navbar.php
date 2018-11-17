78331ad21809ffc5e57e03cf1d49dbeec066bc46
szpatel <szpatel@iu.edu>
2018-11-17 13:14:27 -0500

    Remved links from nav bar

--- php/navbar.php
+++ php/navbar.php
@@ -5,20 +5,6 @@
 				<span class='navbar-toggler-icon'></span>
 			</button>
 
-			<div class='collapse navbar-collapse' id='navbarSupportedContent'>
-				<ul class='navbar-nav mr-auto'>
-					<li class='nav-item'>
-						<a class='nav-link' href='index.php'>Home</a>
-					</li>
-					<li class='nav-item'>
-						<a class='nav-link' href='newlease.php'>New Lease</a>
-					</li>
-					<li class='nav-item active'>
-						<a class='nav-link current-lunar-living-nav-link' href='maintenance.php'>Maintenance <span class='sr-only'>(current)</span></a>
-					</li>
-				</ul>
-			</div>
-
 			<ul class='navbar-nav ml-auto'>
 				<li>
 					<div class='dropdown'>
--- php/signInNavbar.php
+++ php/signInNavbar.php
@@ -3,18 +3,6 @@
 			<button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
 				<span class='navbar-toggler-icon'></span>
 			</button>
-
-			<div class='collapse navbar-collapse' id='navbarSupportedContent'>
-				<ul class='navbar-nav mr-auto'>
-					<li class='nav-item active'>
-						<a class='nav-link current-lunar-living-nav-link' href='index.php'>Home <span class='sr-only'>(current)</span></a>
-					</li>
-					<li class='nav-item'>
-						<a class='nav-link' href='newlease.php'>New Lease</a>
-					</li>
-				</ul>
-			</div>
-
 			<?php
             if(isset($_SESSION['username'])){
 			    echo"<ul class='navbar-nav ml-auto'>";
@@ -22,6 +10,7 @@
 					<div class='dropdown'>
 						<button class='btn btn-secondary dropdown-toggle' type='button' id='dropdownMenuButton' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>Hello, ". $_SESSION['firstName'] ."</button>
 						<div class='dropdown-menu' aria-labelledby='dropdownMenuButton'>
+							<a class='dropdown-item' href='profile.php'>Profile</a>
 							<a class='dropdown-item' href='logout.php'>Signout</a>
 						</div>
 					</div>
