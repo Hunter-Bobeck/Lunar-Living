7f821c1133fa451731507a3a7e69dabceb192299
nishchayagupta <nishchayagupta@gmail.com>
2018-11-30 00:40:36 -0500

    otp page UI improved

--- php/login.php
+++ php/login.php
@@ -1,32 +1,32 @@
 <?php
-	session_start();
-    if(isset($_SESSION['username'])){
-        session_destroy();
-    }
+session_start();
+if (isset($_SESSION['username'])) {
+    session_destroy();
+}
 ?>
 <!doctype html>
 <html lang='en'>
 <link rel='stylesheet' href='../css/login.css'>
 <body class = "background background-mars-in-space">
-    
-    
+
+
     <main>
 <div class='row justify-content-center align-items-center'>
 <form action = 'loginverify.php' method = 'post'>
 <div class="login-wrapper">
   <div class="login-left">
-    <img src="../images/mars-wallpaper.jpg">
+    <img src="../images/mars-wallpaper1.jpg">
     <div class="h1">Enter Lunar Living</div>
   </div>
   <div class="login-right">
     <div class="h2">Login</div>
     <div class="form-group">
       <input type='text' id='login-username' name='loginUsername' placeholder='Username'>
-      <label for="Email">Email</label>    
+      <label for="Email">Email</label>
     </div>
     <div class="form-group">
       <input type='password' id='login-password' name='loginPassword' placeholder='Password'>
-      <label for="Password">Password</label>    
+      <label for="Password">Password</label>
     </div>
     <div class="button-area">
       <button class="btn btn-secondary" value='Login' type='submit' name='loginButtonLogin' id="loginButtonLogin">Login</button>
@@ -39,12 +39,12 @@
 
 <!-- postJS -->
     <script src='../js/bootstrap.min.js'></script>      <!-- Bootstrap JS -->
-    <script type="text/javascript"> 
+    <script type="text/javascript">
     var openLoginRight = document.querySelector('.h1');
 var loginWrapper = document.querySelector('.login-wrapper');
 
 openLoginRight.addEventListener('click', function(){
-  loginWrapper.classList.toggle('open'); 
+  loginWrapper.classList.toggle('open');
 });
     </script>
 
--- php/otpverification.php
+++ php/otpverification.php
@@ -1,33 +1,51 @@
 <!doctype html>
 <html lang='en'>
-<?php include 'header.php'; ?>
-<body class='background background-moon-in-space'>
+<link rel='stylesheet' href='../css/otpverification.css'>
+<!-- <link rel='stylesheet' href='../css/style.css'> -->
+<body class='background background-mars-in-space'>
 	<main>
 		<div id='spacer-otpverification'></div>
 		<div class='container container-form' id='container-otpverification'>
 			<div class='row justify-content-center align-items-center'>
 				<div class='col-md-7' id='login-column'>
-					<div class='text-centered five-padding-bottom'><img src='../images\Logo.png' class='logo'></div>
-					<h5 class='medium-gray text-centered'>OTP Verification</h5>
-					<form class='form' action = 'otpverify.php' method = 'post'>
-						<div class='form-group'>
-							<input class='form-control' type='text' id='otpverification-otp' name='otpverificationOTP' placeholder='One-time password'>
-						</div>
-						<div class='form-group text-centered'>
-							<input class='btn btn-info btn-md' value='Submit' type='submit' name='otpverificationButtonSubmit' id = 'otpverificationButtonSubmit'>
-						</div>
-					</form>
+					<form class="ctr" action = 'otpverify.php' method = 'post'>
+	        	<div class="loader"></div>
+		        <input type='text' id='otpverification-otp' name='otpverificationOTP' >
+		        <div class="indicator" data-content="Enter OTP"></div>
+	        </form>
 				</div>
 			</div>
 		</div>
 		<br>
 		<br>
-		<?php include 'footer.php'; ?>
 	</main>
 
 	<!-- postJS -->
     <script src='../js/bootstrap.min.js'></script>		<!-- Bootstrap JS -->
+  <script>
+  input = document.querySelector "input"
+form = document.querySelector "form"
+indicator = document.querySelector ".indicator"
+loader = document.querySelector ".loader"
+content = input.value
 
+form.addEventListener "submit", (e) ->
+  e.preventDefault()
+  indicator.setAttribute "data-content", "Saving..."
+  loader.classList.add "full"
+  setTimeout (
+    ->
+      indicator.setAttribute "data-content", "You've been subscribed!"
+      loader.classList.add "done"
+      input.classList.add "full"
+      input.value ""
+  ), 3000
+
+input.addEventListener "input", ->
+  indicator.setAttribute "data-content", "Now hit enter!"
+
+balapaCop "Subscribe Form Interaction", "#999"
+  </script>
 
 
 
