6f71717c38d81a3ec5fa9de8b7208dfffe5e450f
nishchayagupta <nishchayagupta@gmail.com>
2018-11-30 16:24:09 -0500

    ticket user UI changed, filter bar fixes

--- php/TicketFilterBars/userFilterBar.php
+++ php/TicketFilterBars/userFilterBar.php
@@ -1,9 +1,9 @@
 <form action = "#">
     <ul style = "list-style-type: none;margin: 0;padding: 0;overflow: hidden;background-color: #333;">
         <li class = "active-filter" style="float:left;display: block;color: white;text-align: center;padding: 22px 16px;text-decoration: none;align:left"><text>Filter</text></li>
-        <li style="float:left;display: block;color: white;text-align: center;padding: 15px 16px;text-decoration: none"><input style = "border: 2px blue;border-radius: 4px;" placeholder = "ID" type = "text" id = "ticket-search-id" name = "ticketsearchid"></li>
-        <li style="float:left;display: block;color: white;text-align: center;padding: 15px 16px;text-decoration: none"><input style = "border: 2px blue;border-radius: 4px;" placeholder = "Title" type = "text" id = "ticket-search-title" name = "ticketsearchtitle"></li>
-        <li style="float:left;display: block;color: white;text-align: center;padding: 15px 16px;text-decoration: none">
+        <li style="float:left;display: block;color: white;text-align: center;padding: 21px 16px;text-decoration: none"><input style = "border: 2px blue;border-radius: 4px;" placeholder = "ID" type = "text" id = "ticket-search-id" name = "ticketsearchid"></li>
+        <li style="float:left;display: block;color: white;text-align: center;padding: 21px 16px;text-decoration: none"><input style = "border: 2px blue;border-radius: 4px;" placeholder = "Title" type = "text" id = "ticket-search-title" name = "ticketsearchtitle"></li>
+        <li style="float:left;display: block;color: white;text-align: center;background-color: #333;padding: 15px 16px;text-decoration: none">
             <div class='dropdown text-centered'>
 				<select class='btn btn-secondary dropdown-toggle' type='button' id='signup-gender' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false' name='signupGender'>
                     <option class='dropdown-item' value="Gender">Status </option>
@@ -14,6 +14,6 @@
                 </select>
 			</div>
         </li>
-        <li style="float:right;display: block;color: white;text-align: center;padding: 15px 16px;text-decoration: none"><input class = "btn-md btn btn-info" value = "Search" type = "submit"></li>
+        <li style="float:right;display: block;color: white;text-align: center;padding: 15px 16px;text-decoration: none;"><input style = 'color:white; background-color: crimson' class = "btn" value = "Search" type = "submit"></li>
     </ul>
 </form>
