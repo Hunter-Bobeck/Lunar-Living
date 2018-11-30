5c6e62ad16f7998c213cda597a82fd9a9b31cf62
nishchayagupta <nishchayagupta@gmail.com>
2018-11-30 17:41:56 -0500

    left padding side bar removed

--- php/review.php
+++ php/review.php
@@ -1,76 +1,76 @@
 <?php
-	session_start();
+session_start();
 ?>
 <!doctype html>
 <html lang='en'>
-<?php include 'header.php'; ?>
+<?php include 'header.php';?>
 <body class="background background-dark">
 	<?php
-		$username = $_SESSION['username'];
-        
-        $ch = curl_init('https://lunar-living.herokuapp.com/getReviews');
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_USERAGENT, 'YourScript/0.1 (contact@email)');
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-			'Content-Type: application/json',
-			"rating: 5"
-			));
-        $data = curl_exec($ch);
-        $info = curl_getinfo($ch);
-		$fiveStar = json_decode($data)->total;
-        curl_close($ch);
-        
-        $ch = curl_init('https://lunar-living.herokuapp.com/getReviews');
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_USERAGENT, 'YourScript/0.1 (contact@email)');
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-			'Content-Type: application/json',
-			"rating: 4"
-			));
-        $data = curl_exec($ch);
-        $info = curl_getinfo($ch);
-		$fourStar = json_decode($data)->total;
-        curl_close($ch);
-        
-        $ch = curl_init('https://lunar-living.herokuapp.com/getReviews');
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_USERAGENT, 'YourScript/0.1 (contact@email)');
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-			'Content-Type: application/json',
-			"rating: 3"
-			));
-        $data = curl_exec($ch);
-        $info = curl_getinfo($ch);
-		$threeStar = json_decode($data)->total;
-        curl_close($ch);
-        
-        $ch = curl_init('https://lunar-living.herokuapp.com/getReviews');
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_USERAGENT, 'YourScript/0.1 (contact@email)');
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-			'Content-Type: application/json',
-			"rating: 2"
-			));
-        $data = curl_exec($ch);
-        $info = curl_getinfo($ch);
-		$twoStar = json_decode($data)->total;
-        curl_close($ch);
-        
-        $ch = curl_init('https://lunar-living.herokuapp.com/getReviews');
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_USERAGENT, 'YourScript/0.1 (contact@email)');
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-			'Content-Type: application/json',
-			"rating: 1"
-			));
-        $data = curl_exec($ch);
-        $info = curl_getinfo($ch);
-		$oneStar = json_decode($data)->total;
-		curl_close($ch);
-	?>
+$username = $_SESSION['username'];
+
+$ch = curl_init('https://lunar-living.herokuapp.com/getReviews');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_USERAGENT, 'YourScript/0.1 (contact@email)');
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json',
+    "rating: 5",
+));
+$data = curl_exec($ch);
+$info = curl_getinfo($ch);
+$fiveStar = json_decode($data)->total;
+curl_close($ch);
+
+$ch = curl_init('https://lunar-living.herokuapp.com/getReviews');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_USERAGENT, 'YourScript/0.1 (contact@email)');
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json',
+    "rating: 4",
+));
+$data = curl_exec($ch);
+$info = curl_getinfo($ch);
+$fourStar = json_decode($data)->total;
+curl_close($ch);
+
+$ch = curl_init('https://lunar-living.herokuapp.com/getReviews');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_USERAGENT, 'YourScript/0.1 (contact@email)');
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json',
+    "rating: 3",
+));
+$data = curl_exec($ch);
+$info = curl_getinfo($ch);
+$threeStar = json_decode($data)->total;
+curl_close($ch);
+
+$ch = curl_init('https://lunar-living.herokuapp.com/getReviews');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_USERAGENT, 'YourScript/0.1 (contact@email)');
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json',
+    "rating: 2",
+));
+$data = curl_exec($ch);
+$info = curl_getinfo($ch);
+$twoStar = json_decode($data)->total;
+curl_close($ch);
+
+$ch = curl_init('https://lunar-living.herokuapp.com/getReviews');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_USERAGENT, 'YourScript/0.1 (contact@email)');
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json',
+    "rating: 1",
+));
+$data = curl_exec($ch);
+$info = curl_getinfo($ch);
+$oneStar = json_decode($data)->total;
+curl_close($ch);
+?>
 	<main class = 'content_body'>
 	<div class='container-fluid padding-zero'>
-        <?php include 'signInNavbar.php'; ?>
+        <?php include 'signInNavbar.php';?>
 	</div>
 	<div class='container-fluid userlease_container'>
 		<div class='row'>
@@ -80,33 +80,33 @@
 						<ul>
                         <li><a href='profile.php'>Profile</a></li>
 							<?php
-							if($_SESSION["usertype"] == 1){
-								echo"<li><a href='user_lease.php'>Lease</a></li>
+if ($_SESSION["usertype"] == 1) {
+    echo "<li><a href='user_lease.php'>Lease</a></li>
 								<li><a href='payment.php'>Payment</a></li>";
-							}
-							if($_SESSION["usertype"] == 2){
-                                echo"<li><a href='newlease.php'>New Lease</a></li>";
-                                echo"<li><a href='allLogin.php'>All Users</a></li>";
-                                echo"<li><a href='allLease.php'>All Leases</a></li>";
-                                echo"<li><a href='appointments.php'>All Appointments</a></li>";
-                                echo"<li><a href='allpromocodes.php'>All Promo Codes</a></li>";
-							}
-							if($_SESSION["usertype"] == 2){
-								echo"<li><a href='adminchat.php'>Chats</a></li>";
-							}
-							?>
+}
+if ($_SESSION["usertype"] == 2) {
+    echo "<li><a href='newlease.php'>New Lease</a></li>";
+    echo "<li><a href='allLogin.php'>All Users</a></li>";
+    echo "<li><a href='allLease.php'>All Leases</a></li>";
+    echo "<li><a href='appointments.php'>All Appointments</a></li>";
+    echo "<li><a href='allpromocodes.php'>All Promo Codes</a></li>";
+}
+if ($_SESSION["usertype"] == 2) {
+    echo "<li><a href='adminchat.php'>Chats</a></li>";
+}
+?>
 							<li><a href='ticketStatus.php'>Tickets</a></li>
 							<?php
-							if($_SESSION["usertype"] == 2){
-								echo"<li><a href='map.php'>Ticket Map</a></li>";
-							}
-							?>
+if ($_SESSION["usertype"] == 2) {
+    echo "<li><a href='map.php'>Ticket Map</a></li>";
+}
+?>
 							<li><a href='events.php'>Events</a></li>
 							<li><a href='laundry.php'>Laundry</a></li>
 							<li class='active'><a href='review.php'>Review</a></li>
 							<?php
-							if($_SESSION["usertype"] == 2){
-								echo"<li>
+if ($_SESSION["usertype"] == 2) {
+    echo "<li>
 									<a onclick='displayStats()' href='#'>Stats</a>
 									<ul id='statsChilds' class= 'statsChilds'>
 										<li><a href='paymentstats.php'>Payment Stats</a></li>
@@ -114,8 +114,8 @@
 										<li><a href='ticketstatsstatus.php'>Ticket Status Stats</a></li>
 									</ul>
 								</li>";
-							}
-							?>
+}
+?>
 						</ul>
 					</aside>
 				</div>
@@ -135,58 +135,52 @@
                 <br>
                 <div class='user-review'>
                     <?php
-                    $totalVotes = $fiveStar + $fourStar + $threeStar + $twoStar + $oneStar;
-                    $sum = 5 * $fiveStar + 4 * $fourStar + 3 * $threeStar + 2 * $twoStar + $oneStar;
-                    if($totalVotes == 0){
-                        $avg = 0;
-                        $fiveWidth = 0;
-                        $fourWidth = 0;
-                        $threeWidth = 0;
-                        $twoWidth = 0;
-                        $oneWidth = 0;
-                    }
-                    else{
-                        $avg = $sum / $totalVotes;
-                        $fiveWidth = ($fiveStar/$totalVotes) * 100;
-                        $fourWidth = ($fourStar/$totalVotes) * 100;
-                        $threeWidth = ($threeStar/$totalVotes) * 100;
-                        $twoWidth = ($twoStar/$totalVotes) * 100;
-                        $oneWidth = ($oneStar/$totalVotes) * 100;
-                    }
-                    echo"
+$totalVotes = $fiveStar + $fourStar + $threeStar + $twoStar + $oneStar;
+$sum = 5 * $fiveStar + 4 * $fourStar + 3 * $threeStar + 2 * $twoStar + $oneStar;
+if ($totalVotes == 0) {
+    $avg = 0;
+    $fiveWidth = 0;
+    $fourWidth = 0;
+    $threeWidth = 0;
+    $twoWidth = 0;
+    $oneWidth = 0;
+} else {
+    $avg = $sum / $totalVotes;
+    $fiveWidth = ($fiveStar / $totalVotes) * 100;
+    $fourWidth = ($fourStar / $totalVotes) * 100;
+    $threeWidth = ($threeStar / $totalVotes) * 100;
+    $twoWidth = ($twoStar / $totalVotes) * 100;
+    $oneWidth = ($oneStar / $totalVotes) * 100;
+}
+echo "
                     <span class='heading'>User Rating &nbsp</span>";
-                    if($avg >= 1){
-                        echo "<span class='fa fa-star checked'></span>";
-                    }
-                    else{
-                        echo "<span class='fa fa-star'></span>";
-                    }
-                    if($avg >= 2){
-                        echo "<span class='fa fa-star checked'></span>";
-                    }
-                    else{
-                        echo "<span class='fa fa-star'></span>";
-                    }
-                    if($avg >= 3){
-                        echo "<span class='fa fa-star checked'></span>";
-                    }
-                    else{
-                        echo "<span class='fa fa-star'></span>";
-                    }
-                    if($avg >= 4){
-                        echo "<span class='fa fa-star checked'></span>";
-                    }
-                    else{
-                        echo "<span class='fa fa-star'></span>";
-                    }
-                    if($avg >= 5){
-                        echo "<span class='fa fa-star checked'></span>";
-                    }
-                    else{
-                        echo "<span class='fa fa-star'></span>";
-                    }
-                    echo"
-                    <p>". $avg . " average based on ". $totalVotes . " reviews.</p>
+if ($avg >= 1) {
+    echo "<span class='fa fa-star checked'></span>";
+} else {
+    echo "<span class='fa fa-star'></span>";
+}
+if ($avg >= 2) {
+    echo "<span class='fa fa-star checked'></span>";
+} else {
+    echo "<span class='fa fa-star'></span>";
+}
+if ($avg >= 3) {
+    echo "<span class='fa fa-star checked'></span>";
+} else {
+    echo "<span class='fa fa-star'></span>";
+}
+if ($avg >= 4) {
+    echo "<span class='fa fa-star checked'></span>";
+} else {
+    echo "<span class='fa fa-star'></span>";
+}
+if ($avg >= 5) {
+    echo "<span class='fa fa-star checked'></span>";
+} else {
+    echo "<span class='fa fa-star'></span>";
+}
+echo "
+                    <p>" . $avg . " average based on " . $totalVotes . " reviews.</p>
                     <hr style='border:3px solid #f1f1f1'>
                     <div class='row'>
                     <div class='side'>
@@ -194,68 +188,68 @@
                     </div>
                     <div class='middle'>
                         <div class='bar-container'>
-                        <div class='bar-5' style='width:". $fiveWidth ."%;'></div>
+                        <div class='bar-5' style='width:" . $fiveWidth . "%;'></div>
                         </div>
                     </div>
                     <div class='side right'>
-                        <div>". $fiveStar ."</div>
+                        <div>" . $fiveStar . "</div>
                     </div>
                     <div class='side'>
                         <div>4 star</div>
                     </div>
                     <div class='middle'>
                         <div class='bar-container'>
-                        <div class='bar-4' style='width:". $fourWidth ."%;'></div>
+                        <div class='bar-4' style='width:" . $fourWidth . "%;'></div>
                         </div>
                     </div>
                     <div class='side right'>
-                        <div>". $fourStar ."</div>
+                        <div>" . $fourStar . "</div>
                     </div>
                     <div class='side'>
                         <div>3 star</div>
                     </div>
                     <div class='middle'>
                         <div class='bar-container'>
-                        <div class='bar-3' style='width:". $threeWidth ."%;'></div>
+                        <div class='bar-3' style='width:" . $threeWidth . "%;'></div>
                         </div>
                     </div>
                     <div class='side right'>
-                        <div>". $threeStar ."</div>
+                        <div>" . $threeStar . "</div>
                     </div>
                     <div class='side'>
                         <div>2 star</div>
                     </div>
                     <div class='middle'>
                         <div class='bar-container'>
-                        <div class='bar-2' style='width:". $twoWidth ."%;'></div>
+                        <div class='bar-2' style='width:" . $twoWidth . "%;'></div>
                         </div>
                     </div>
                     <div class='side right'>
-                        <div>". $twoStar ."</div>
+                        <div>" . $twoStar . "</div>
                     </div>
                     <div class='side'>
                         <div>1 star</div>
                     </div>
                     <div class='middle'>
                         <div class='bar-container'>
-                        <div class='bar-1' style='width:". $oneWidth ."%;'></div>
+                        <div class='bar-1' style='width:" . $oneWidth . "%;'></div>
                         </div>
                     </div>
                     <div class='side right'>
-                        <div>". $oneStar ."</div>
+                        <div>" . $oneStar . "</div>
                     </div>";
-                    ?>
+?>
                 </div>
 			</div>
 		</div>
 	</div>
-    <?php include 'footer.php'; ?>
+    <?php include 'footer.php';?>
 	</main>
 
 	<!-- postJS -->
     <script src='../js/jquery-3.3.1.js'></script>		<!-- Jquery JS (necessary for dropdowns) -->
     <script src='../js/bootstrap.bundle.min.js'></script>		<!-- Bootstrap Bundle JS (necessary for dropdowns) -->
-    <!-- <script src='js/bootstrap.min.js'></script> -->		<!-- Bootstrap JS � disabled because when enabled it has a conflict with Bootstrap Bundle JS that makes dropdowns require two clicks to dropdown; it doesn't seem that any needed functionality is lacking when this is disabled -->	
+    <!-- <script src='js/bootstrap.min.js'></script> -->		<!-- Bootstrap JS � disabled because when enabled it has a conflict with Bootstrap Bundle JS that makes dropdowns require two clicks to dropdown; it doesn't seem that any needed functionality is lacking when this is disabled -->
     <script>
         function rateMe(){
             var starValue = $("input[name='rating']:checked").val();
