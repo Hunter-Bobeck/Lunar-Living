192218460d9ebe0994dd907d73de2d6f9bb79dc6
szpatel <szpatel@iu.edu>
2018-12-03 12:29:06 -0500

    Ui modification admin side

--- php/chatAdmin.php
+++ php/chatAdmin.php
@@ -7,7 +7,7 @@ if(isset($_GET['logout'])){
 ?>
 <div id="wrapper" class='chat'>
     <div id="menu">
-        <p class="welcome">Welcome, <b><?php echo $_SESSION['firstName']; ?></b></p>
+        <h3 class="welcome">Welcome, <b><?php echo $_SESSION['firstName']; ?></b></h3>
         <div style="clear:both"></div>
     </div>    
     <div id="chatbox" class='message'>
